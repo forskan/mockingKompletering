58600049415f8e2a530d13286b8d5f2d70a6f47b
Robert Forsgren <rf222ce@student.lnu.se>
2015-01-11 22:29:05 +0100

    Revert "GREEN"
    This reverts commit 016a56e36c819c073ee9bc8cf3dd6989bcc68e50.

--- src/Test/PersonTests.java
+++ src/Test/PersonTests.java
@@ -1,7 +1,6 @@
 package Test;
 
 import org.junit.Test;
-import org.mockito.InOrder;
 
 import WalkingPeople.Leg;
 import WalkingPeople.Person;
@@ -32,44 +31,6 @@ public class PersonTests {
 	}
 		
 	@Test
-	public void testWalkMore(){
-		Leg mockedLeg1 = mock(Leg.class);	
-		Leg mockedLeg2 = mock(Leg.class);
-		Person person = new Person(mockedLeg1, mockedLeg2);
-		person.walk(9);
-		verify(mockedLeg1, times(5)).takeStep();
-		verify(mockedLeg2, times(4)).takeStep();
-	}
-	
-	// inOrder.verify(firstMock).add("was called first");
-	 //inOrder.verify(secondMock).add("was called second");
-	
-	@Test
-	public void testWalkMoreInOrder(){
-		Leg mockedLeg1 = mock(Leg.class);	
-		Leg mockedLeg2 = mock(Leg.class);
-		Person person = new Person(mockedLeg1, mockedLeg2);
-		person.walk(2);
-		InOrder inOrder = inOrder(mockedLeg1, mockedLeg2);
-		inOrder.verify(mockedLeg1).takeStep();
-		inOrder.verify(mockedLeg2).takeStep();
-		
-	}
-	
-	@Test
-	public void testOnlyTakeOneStep(){
-		Leg mockedLeg1 = mock(Leg.class);	
-		Leg mockedLeg2 = mock(Leg.class);
-		Person person = new Person(mockedLeg1, mockedLeg2);
-		person.walk(1);
-		
-		verify(mockedLeg1).takeStep();
-		verifyZeroInteractions(mockedLeg2);
-		
-	}
-	
-	
-	@Test
 	public void testWalkOnRealObjects(){
 		Person p = new Person(new Leg(), new Leg());
 		p.walk(1);
@@ -82,6 +43,5 @@ public class PersonTests {
 		p.walk(12);
 		assertEquals(6, p.getPosition(), 0);
 	}
-	
 	
 }
