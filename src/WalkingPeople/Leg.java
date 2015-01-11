668eb78f45e5fd7bbd5bb266c8cb5b0a60c0b389
Robert Forsgren <rf222ce@student.lnu.se>
2015-01-11 21:24:02 +0100

    GREEN

--- src/WalkingPeople/Leg.java
+++ src/WalkingPeople/Leg.java
@@ -2,6 +2,11 @@ package WalkingPeople;
 
 public class Leg {
 
-
+	private float m_position = 0.0f;
+	
+	public float getPosition()
+	{
+		return m_position;
+	}
 	
 }
--- src/WalkingPeople/Person.java
+++ src/WalkingPeople/Person.java
@@ -4,6 +4,24 @@ import java.util.Vector;
 
 public class Person {
 
+	private Leg m_leftLeg;
+	private Leg m_rightLeg;
+	
+
+	public float getPosition()
+	{
+		float position = m_leftLeg.getPosition();
+		if(position < m_rightLeg.getPosition()){
+			return m_rightLeg.getPosition();
+		}
+		return position;
+	}
+	
+	public Person(Leg a_legOne, Leg a_legTwo)
+	{
+		m_leftLeg = a_legOne;
+		m_rightLeg = a_legTwo;
+	}
 
 	
 }
